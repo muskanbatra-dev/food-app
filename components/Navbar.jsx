85596296cf5322d578678a5d7c32306a32f89dba
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2022-05-09 13:21:48 +0530

    update

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../styles/Navbar.module.css';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+      <div className={styles.callButton}>
+        <Image src="/img/telephone.png" width="32" height="32" alt=''/>
+      </div>
+      </div>
+      <div className={styles.texts}>
+      <div className={styles.text}>ORDER NOW!</div>
+      <div className={styles.text}>7982399512</div>
+      </div>
+      <div className={styles.item}></div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      
       <Head>
         <title>Go Pizza</title>
         <meta name="description" content="Best pizza delivery service" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"  />
       </Head>
 
       homepage
